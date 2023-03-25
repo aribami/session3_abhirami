e238bf391575339c3fca88df3b1b5e3acb98389d
alekhya399 <125812319+alekhya399@users.noreply.github.com>
2023-03-25 14:33:27 +0530

    freelancer code

--- freelancer.py
+++ freelancer.py
@@ -1,1 +1,12 @@
-print("hello world")
+class freelancer:
+    def dayrate(self, n):
+        return round(n*8)
+    def monthrate(self,n,m):
+        return round(n*8*22*(1-m))
+    def daysinbudget(self,n,m,a):
+        return round(n/(8*m*(1-a)))
+
+p=freelancer()
+print("The day rate =",p.dayrate(89))
+print("The month rate =",p.monthrate(89,0.42))
+print("The number of working days =",p.daysinbudget(20000,89,0.2002))
